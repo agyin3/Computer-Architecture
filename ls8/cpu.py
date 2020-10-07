b2d2b5f052ec38aa8a5a7a5faa7587f426b49ffa
Buddy Agyin <agyin3@gmail.com>
2020-10-06 18:50:38 -0600

    complete challenges for print8 and mul8

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,35 +2,75 @@
 
 import sys
 
+HLT = 0b00000001
+LDI = 0b10000010
+PRN = 0b01000111
+MUL = 0b10100010
+
 
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
         """Construct a new CPU."""
-        self.memory = [0] * 256
-        self.register = [0] * 8
+        self.ram = [0] * 256
+        self.reg = [0] * 8
         self.pc = 0
-        self.ir = None
-        self.mar = None
-        self.mdr = None
+        self.running = False
 
-    def load(self):
+        # create branch table
+        self.branchtable = {}
+        self.branchtable[HLT] = self.handle_HLT
+        self.branchtable[LDI] = self.handle_LDI
+        self.branchtable[PRN] = self.handle_PRN
+        self.branchtable[MUL] = self.handle_MUL
+
+    def ram_read(self, mar):
+        return self.ram[mar]
+
+    def ram_write(self, mar, mdr):
+        self.ram[mar] = mdr
+
+    def handle_HLT(self):
+        self.running = False
+        self.pc = 0
+
+    def handle_LDI(self):
+        reg_idx = self.ram_read(self.pc + 1)
+        value = self.ram_read(self.pc + 2)
+
+        self.reg[reg_idx] = value
+
+        self.pc += 3
+
+    def handle_PRN(self):
+        reg_idx = self.ram_read(self.pc + 1)
+        value = self.reg[reg_idx]
+
+        print(value)
+
+        self.pc += 2
+
+    def handle_MUL(self):
+        reg_idx_a = self.ram_read(self.pc + 1)
+        reg_idx_b = self.ram_read(self.pc + 2)
+
+        self.alu("MUL", reg_idx_a, reg_idx_b)
+        self.pc += 3
+
+    def load(self, file):
         """Load a program into memory."""
 
+        program = []
+        file_obj = open(file, 'r')
+        for line in file_obj:
+            if line == '\n' or line[0] == '#':
+                continue
+            else:
+                integer = int(line[0:8], 2)
+                program.append(integer)
+
         address = 0
-
-        # For now, we've just hardcoded a program:
-
-        program = [
-            # From print8.ls8
-            0b10000010,  # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111,  # PRN R0
-            0b00000000,
-            0b00000001,  # HLT
-        ]
 
         for instruction in program:
             self.ram[address] = instruction
@@ -42,6 +82,10 @@ class CPU:
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
         # elif op == "SUB": etc
+
+        elif op == "MUL":
+            self.reg[reg_a] *= self.reg[reg_b]
+
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -67,4 +111,36 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        pass
+        self.running = True
+
+        while self.running:
+            ir = self.ram_read(self.pc)
+            self.branchtable[ir]()
+
+            # OLD CODE 0(N)
+            # if ir == LDI:
+            #     reg_idx = self.ram_read(self.pc + 1)
+            #     value = self.ram_read(self.pc + 2)
+
+            #     self.reg[reg_idx] = value
+
+            #     self.pc += 3
+
+            # elif ir == PRN:
+            #     reg_idx = self.ram_read(self.pc + 1)
+            #     value = self.reg[reg_idx]
+
+            #     print(value)
+
+            #     self.pc += 2
+
+            # elif ir == MUL:
+            #     reg_idx_a = self.ram_read(self.pc + 1)
+            #     reg_idx_b = self.ram_read(self.pc + 2)
+
+            #     self.alu("MUL", reg_idx_a, reg_idx_b)
+            #     self.pc += 3
+
+            # elif ir == HLT:
+            #     self.pc = 0
+            #     running = False
--- ls8/ls8.py
+++ ls8/ls8.py
@@ -5,7 +5,9 @@
 import sys
 from cpu import *
 
+data = sys.argv[1]
+
 cpu = CPU()
 
-cpu.load()
+cpu.load(data)
 cpu.run()
